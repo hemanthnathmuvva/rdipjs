d45763aa2db7f3cbf468d3466bf093c9cd0c7c43
hemanthnathmuvva <70476433+hemanthnathmuvva@users.noreply.github.com>
2020-09-11 20:36:15 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,53 @@
+function dis(val)
+         {
+             document.getElementById("result").value+=val
+         }
+           
+         function solve()
+         {
+             let x = document.getElementById("result").value
+             let y = eval(x)
+             document.getElementById("result").value = y
+         }
+           
+         function clr()
+         {
+             document.getElementById("result").value = ""
+         } 
+
+function form()
+{
+    var regName = /^[a-zA-Z]+$/;
+    var name = document.getElementById('fname').value;
+    if(!name.match(regName)){
+        alert('Cannot take a number as starting letter.');
+        document.getElementById('fname').focus();
+        return false;
+
+    }else{
+        alert('Valid name given.');
+        return true;
+    }
+     
+}
+
+
+function palindrome("str")
+{
+a=true;
+for(let i=0; j=str.length-1;i<=j;i++,j--)
+{
+if(str[i] !==str[j])
+{
+a=false;
+break;
+}
+}
+return a;
+}
+function valid()
+{
+r=document.getElementById("t").value;
+b=palindrome(str);
+alert(b);
+}
